a2cac0b36b3fadde9edb36a3f2c8bb874d92c0d3
kai elijah gronholz <kaielijah2113@gmail.com>
2025-09-30 10:07:24 -0500

    prompted the user to enter their billing and shipping adress, then displays the inputs in a specific format.

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,68 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
+
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        StringBuilder adressInfo = new StringBuilder();
+
+        System.out.println("Enter your billing and shipping adress.");
+
+        //Get Billing
+        System.out.println("Full name: ");
+        String fullName = input.nextLine();
+
+        System.out.println("Billing street: ");
+        String billingStreet = input.nextLine();
+
+        System.out.println("Billing City: ");
+        String billingCity = input.nextLine();
+
+        System.out.println("Billing State: ");
+        String billingState = input.nextLine();
+
+        System.out.println("Billing Zip: ");
+        String billingZip = input.nextLine();
+
+        //Get Shipping
+
+
+        System.out.println("Shipping street: ");
+        String shippingStreet = input.nextLine();
+
+        System.out.println("Shipping City: ");
+        String shippingCity = input.nextLine();
+
+        System.out.println("Shipping State: ");
+        String shippingState = input.nextLine();
+
+        System.out.println("Shipping Zip: ");
+        String shippingZip = input.nextLine();
+
+
+        //formating output
+        adressInfo.append("Adress Information");
+            adressInfo.append("===================\\n\\n");
+            adressInfo.append("Full name: ").append(fullName).append("\n\n");
+            adressInfo.append("Billing Adress\n");
+            adressInfo.append("----------------\n");
+            adressInfo.append("Street: ").append(billingStreet).append("\n");
+            adressInfo.append("City: ").append(billingCity).append("\n");
+            adressInfo.append("Zip: ").append(billingZip).append("\n\n");
+            adressInfo.append("Shipping Adress\n");
+            adressInfo.append("----------------\n");
+            adressInfo.append("Street: ").append(shippingStreet).append("\n");
+            adressInfo.append("City: ").append(shippingCity).append("\n");
+            adressInfo.append("State: ").append(shippingState).append("\n");
+            adressInfo.append("Zip: ").append(shippingZip).append("\n");
+
+        System.out.println("\n" + adressInfo.toString());
+
+
+
+
+    }
 }
